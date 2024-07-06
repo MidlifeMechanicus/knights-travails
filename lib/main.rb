56461de25bc788ede15ef6aa40fb5b401f1d356a
ElectronDragon <electricdragondevelopment@gmail.com>
2024-07-06 09:36:00 +0100

    Refactor; Run rubocop -a

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,7 @@
+# See Project notes in README for more information about this approach.
 require_relative "main/board_square"
 
-def knight_moves (start, finish)
+def knight_moves(start, finish)
   queue = []
   current_square = BoardSquare.new(start)
 
@@ -9,16 +10,12 @@ def knight_moves (start, finish)
       queue << BoardSquare.new(square, current_square)
     end
     current_square = queue.shift
-
   end
-
   current_square.display_previous
-
   current_square.reset
-
 end
 
-knight_moves([0,0],[3,3])
-knight_moves([3,3],[0,0])
-knight_moves([0,0],[7,7])
-knight_moves([3,3],[4,3])
+knight_moves([0, 0], [3, 3])
+knight_moves([3, 3], [0, 0])
+knight_moves([0, 0], [7, 7])
+knight_moves([3, 3], [4, 3])
--- lib/main/board_square.rb
+++ lib/main/board_square.rb
@@ -1,4 +1,4 @@
-# Class creates vertices for Gameboard
+# Class for creating nodes on a gameboard as knight (playing piece) moves through them.
 class BoardSquare
   attr_accessor :coordinates, :previous_square, :next_squares
 
@@ -10,23 +10,23 @@ class BoardSquare
     @next_squares = []
     @@history << @coordinates
 
-    add_move([coord_array[0] -2, coord_array[1] +1])
-    add_move([coord_array[0] -1, coord_array[1] +2])
-    add_move([coord_array[0] +1, coord_array[1] +2])
-    add_move([coord_array[0] +2, coord_array[1] +1])
-    add_move([coord_array[0] +2, coord_array[1] -1])
-    add_move([coord_array[0] +1, coord_array[1] -2])
-    add_move([coord_array[0] -1, coord_array[1] -2])
-    add_move([coord_array[0] -2, coord_array[1] -1])
+    add_move([coord_array[0] - 2, coord_array[1] + 1])
+    add_move([coord_array[0] - 1, coord_array[1] + 2])
+    add_move([coord_array[0] + 1, coord_array[1] + 2])
+    add_move([coord_array[0] + 2, coord_array[1] + 1])
+    add_move([coord_array[0] + 2, coord_array[1] - 1])
+    add_move([coord_array[0] + 1, coord_array[1] - 2])
+    add_move([coord_array[0] - 1, coord_array[1] - 2])
+    add_move([coord_array[0] - 2, coord_array[1] - 1])
 
+    # This could have been done more elegantly with an array and #map. But it works.
   end
 
   def add_move(move)
-    if move[0] >= 0 && move[1] >= 0 && move[0] <= 7 && move[1] <= 7
-      unless @@history.include?(move)
-        @next_squares << move
-      end
-    end
+    return unless (0..7).include?(move[0]) && (0..7).include?(move[1])
+    return if @@history.include?(move)
+
+    @next_squares << move
   end
 
   def display_previous
@@ -36,11 +36,10 @@ class BoardSquare
       previous << current_square.coordinates
       current_square = current_square.previous_square
     end
-    p previous.reverse()
+    p previous.reverse
   end
 
   def reset
     @@history = []
   end
-
 end
